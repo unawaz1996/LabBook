10afaf52e612f142f2fe412ea89aa8bef2cf85e3
Urwah Nawaz <urwahnawaz@Urwahs-MBP.gateway>
2020-02-01 20:22:45 +1030

    changes for phoenix

---
+++ NeuroScore/Autism/analysis_phoenix.R
@@ -0,0 +1,18 @@
+suppressPackageStartupMessages({
+  library(magrittr)
+  library(data.table)
+  library(dplyr)
+  library(tibble)
+  library(stringr)
+  library(Biobase)
+  library(readxl)
+  library(gdata)
+  library(Matrix)
+  library(SingleCellExperiment)
+  library(scater)
+  library(readr)
+})
+
+
+sce <- readRDS("sce.rds")
+
