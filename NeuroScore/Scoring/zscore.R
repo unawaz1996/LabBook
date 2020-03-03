ee1dc31dcc5b170b655886732e9d0fddbfaa4523
unawaz1996 <a1654797@adelaide.edu.au>
2020-03-03 13:25:42 +1030

    added comments

--- NeuroScore/Scoring/zscore.R
+++ NeuroScore/Scoring/zscore.R
@@ -43,46 +43,77 @@ zscore <- data.frame(matrix())
 
 
 ### this one involves summing all counts 
-for (i in ASD_list) {
+zscore_ASD = function(ASD_list, counts, meta) {
+  
+  ### Loading all the necessary libraries for this analysis 
+  suppressPackageStartupMessages({
+    library(magrittr)
+    library(Matrix)
+    library(data.table)
+    library(gdata)
+    library(DT)
+    library(tidyverse)
+    library(edgeR)
+  })
+  
+  counts <- counts 
+  counts %<>%  ## converting dgTMatrix option into a dataframe 
+    as.matrix() %>%
+    as.data.frame() %>% 
+    cpm()
+  zscore <- data.frame(matrix())
+  for (i in ASD_list) {
   ### subsetting for the first individual
   ### get all the information out from the meta data 
   ## by using dplyr we are retriving all cells that belong to an ASD individual
   ## and controls 
-  name <- i
-  
-  message("subsetting from meta data for"," ", i)
-  
-  meta_ASD <- meta %>% 
-    rownames_to_column("sample_names") %>%
-    dplyr::filter(individual == name | diagnosis == "Control") %>% 
-    column_to_rownames("sample_names")    
+    name <- i
+    message("subsetting from meta data for"," ", i)
+    meta_ASD <- meta %>% 
+      rownames_to_column("sample_names") %>%
+      dplyr::filter(individual == name | diagnosis == "Control") %>% 
+      column_to_rownames("sample_names")    
   
   ### subset the counts based on the ASD meta data 
   
-  message("Subsetting counts for"," ",i)
+  message("Subsetting CPM normalised counts for"," ",i)
+  
   counts_ASD <- counts %>% 
     as.data.frame() %>% 
     dplyr::select(as.character(rownames(meta_ASD)))
+  
+  ### creating an Id list
+  ## this id list contains informaion of individual and clusters
+  ## both of which will be used to sum counts (i.e counts for cells that belong to a cluster 
+  ## from a specific individual will be summed)
   
   idList <- meta_ASD %>% 
     rownames_to_column("cellID") %>%
     mutate(f = paste(cluster, individual, sep = "_")) %>% 
     split(f = .$f) %>% lapply(extract2, "cellID")    ### by using lapply we sum the counts 
   
-  message("Now summing counts based on individual and celltype for"," ", i)
+  message("Now summing CPM counts based on individual and celltype for"," ", i)
   
   counts_summed <- lapply(idList, function(x){rowSums(counts[,x])}) 
-  counts_summed <- as.data.frame(do.call(rbind, counts_summed))
-  counts_summed <- as.data.frame(t(counts_summed[,-1]))    ## retrieve cell-type information from this
+  ## lapply returns a list, so to flatten the list we use the following function
+  counts_summed <- as.data.frame(do.call(rbind, counts_summed)) 
+  counts_summed <- as.data.frame(t(counts_summed[,-1]))    
   
+  ## retrieve cell-type information from this
   patterns <- unique(sub("_.*", "", colnames(counts_summed)))    
   
   message("Calculating zscore for each celltype for"," ", i)
+  ## we want to calculate zscores for each cell type for each ASD individual 
+  ## so we subset cells based if they belong to a specific cell-type
+  
   for (m in patterns){
     cell_type <- counts_summed %>% 
       dplyr::select(matches(m))
+    
     zscore_calc_cellType <- as.data.frame(t(scale(cell_type)))
-        
+    
+    # we are just interested in the zscores for the ASD individual so we susbet that result 
+    # and save into a DF
     zscore_results <- as.data.frame(t(zscore_calc_cellType)) %>%
       dplyr::select(matches(as.character(i)))
     message("zscore calculate for cluster", " ", m)
@@ -91,7 +122,7 @@ for (i in ASD_list) {
   
   message("All finished for"," ", i, ":)")
 }
-
+}
 
 write.csv(zscore, "zscores_summed.csv")
 
